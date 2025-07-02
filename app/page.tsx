6af680f76dd343eac55795d7933222da68e971db
WIZZLS <swerkxd@gmail.com>
2025-07-02 22:39:10 +0200

    fix: remove extra font and duplicate getBalance

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
 import Balance from "./components/Balance"; // путь поправь, если не работает
-import { getBalance } from "./getBalance";   // путь тоже проверь
 import { getBalance } from "./utils/getBalance";
 
 
